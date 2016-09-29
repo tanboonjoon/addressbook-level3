790aa3a5f8a41e89b7eda5c7d40dcbea1c1813ea
tanboonjoon <tbj_999@hotmail.com>
2016-09-29 23:01:51 +0800

    Apply DIP to logic, forgot to save previously

--- src/seedu/addressbook/storage/IStorageFile.java
+++ src/seedu/addressbook/storage/IStorageFile.java
@@ -1,5 +1,17 @@
 package seedu.addressbook.storage;
 
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
 public interface IStorageFile {
+	
+	
+	 public void save(AddressBook addressBook) throws StorageOperationException;
+	 
+	 public AddressBook load() throws StorageOperationException;
+	 
+	 public String getPath();
+	 
+	 
 
 }
